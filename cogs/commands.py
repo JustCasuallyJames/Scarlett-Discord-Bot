b014d3cc76f06307ab4927d3c83264c65b772a9a
James <jamesvo801@gmail.com>
2019-12-30 23:33:14 -0800

    added add/delete roles
    -the add/delete_role() will be able to add/delete roles and will only allow it if the given member has the role "Moderators".
    
    -Deleted some lines of code because they were useless due to the @commands.has_role()

--- cogs/commands.py
+++ cogs/commands.py
@@ -62,23 +62,24 @@ class Commands(commands.Cog):
             await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
 
     @commands.command(aliases=["changeprefix"])
+    @commands.has_role("Moderators")
     async def changePrefix(self, ctx, prefix):
         with open('cogs/prefixes.json', 'r') as f:
             prefixes = json.load(f)
 
-        role = discord.utils.get(ctx.author.roles, name="Moderators")
-        if role in ctx.author.roles:
-            if len(prefix) == 1:
-                prefixes[str(ctx.guild.id)] = prefix
-            else:
-                await ctx.send(f"Hey you can't put a prefix that's more than one character!")
+        # role = discord.utils.get(ctx.author.roles, name="Moderators")
+        # if role in ctx.author.roles:
+        if len(prefix) == 1:
+            prefixes[str(ctx.guild.id)] = prefix
         else:
-            await ctx.send(f"Unfortunately, you can't change the prefix.")
+            await ctx.send("You can't put a prefix that's more than one character!")
+        # else:
+        #     await ctx.send(f"Unfortunately, you can't change the prefix.")
 
         with open('cogs/prefixes.json', 'w') as f:
             json.dump(prefixes, f, indent=4)
         embed = discord.Embed(
-            description=f"The current prefix is: {prefixes[str(ctx.guild.id)]}",
+            description=f"The changed prefix is: {prefixes[str(ctx.guild.id)]}",
             colour=discord.Colour.purple()
         )
         # embed.set_author(name="Scarlett",
@@ -100,18 +101,30 @@ class Commands(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command(aliases=["addrole"])
-    @commands.is_owner()
+    @commands.has_role("Moderators")
     # The * causes role to hold the user input text after the member name, including any interior whitespace.
     # This lets the user write the following to add the "My Little Pony" role to hogarth: .addrole @hogarth My Little Pony
     async def add_roles(self, ctx, member: discord.Member, *, role: discord.Role):
         # role = discord.utils.get(member.guild.roles, name=role_name)
         await member.add_roles(role)
-        # sends the user a DM
-        await member.send(f"You've been awarded the **{role}** role")
+        # await member.send(f"You've been awarded the **{role}** role")
+        embed = discord.Embed(
+            title="**Role added**",
+            description=f"{member.mention} awarded the **{role}** role",
+            colour=discord.Colour.red()
+        )
+        await ctx.send(embed=embed)
 
-    # @commands.command()
-    # async def test(self,ctx,*,args):
-    #     await ctx.send('You passed {} arguments: {}'.format(len(args), ', '.join(args)))
+    @commands.command(aliases=["delrole"])
+    @commands.has_role("Moderators")
+    async def del_roles(self, ctx, member: discord.Member, *, role: discord.Role):
+        await member.remove_roles(role)
+        embed = discord.Embed(
+            title="**Role deleted**",
+            description=f"{role} role has been revoked from {member.mention}",
+            colour=discord.Colour.red()
+        )
+        await ctx.send(embed=embed)
 
 
 def setup(client):
