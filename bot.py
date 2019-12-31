154f57aa83b57b84ef2619381f3bb29926e94891
James <jamesvo801@gmail.com>
2019-12-31 02:44:28 -0800

    added reload function
    -now there's a reload function so that the unload and load function isn't necessary. 
    
    -added security issues such as the checks

--- bot.py
+++ bot.py
@@ -23,16 +23,24 @@ client.remove_command('help')
 
 
 @client.command()
+@commands.is_owner()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
     await ctx.send(f"Loaded {extension}")
 
 
 @client.command()
+@commands.is_owner()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     await ctx.send(f"unloaded {extension}")
 
+
+@client.command()
+@commands.is_owner()
+async def reload(ctx, extension):
+    client.reload_extension(f'cogs.{extension}')
+    await ctx.send(f"Reloaded: {extension}")
 
 for filename in os.listdir("./cogs"):
     if filename.endswith('.py'):
