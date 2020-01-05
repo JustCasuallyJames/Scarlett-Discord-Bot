448d221567d14ddc48f205b3cb08841076eea9ac
James <jamesvo801@gmail.com>
2020-01-05 03:39:37 -0800

    Fixed bugs
    -Have to message twice to level up.
    -Bug: have to message one more time to get level up message

--- cogs/Levels.py
+++ cogs/Levels.py
@@ -8,11 +8,14 @@ decimal.getcontext().prec = 28
 
 
 def xptolvl(xp):
-    # number = (((5 / 4) * xp) ** 1 / 3) * 1000000000000000
-    # whole_number = math.floor(((5 / 4) * xp) ** 1 / 3) * 1000000000000000
-    # if number - whole_number == 999999999999999:
+    # number = ((((5 / 4) * xp) ** (1 / 3)) * 1000)/1000
+    #
+    # whole_number = math.floor(((5 / 4) * xp) ** (1 / 3)) * 1000
+    # print(f"decimal_total: {number}")
+    # if number % 1000 >= 999:
     #     print("working......")
-    #     return math.ceil(((5 / 4) * xp) ** .3333333333333)
+    #     return round(((5 / 4) * xp) ** (1/3))
+
     return math.floor(((5/4)*xp)**(1/3))
 
 
@@ -26,7 +29,10 @@ class Levels(commands.Cog):
         self.client = client
 
     def is_lvl_up(self, xp, lvl):
-        return xp == lvltoxp(lvl)
+        # print(f"xp: {xp}")
+        # print(f"LVLTOXP: {lvltoxp(lvl)}")
+        # print(xp == lvltoxp(lvl))
+        return xp == lvltoxp(lvl)+1
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -38,7 +44,7 @@ class Levels(commands.Cog):
         xp = await self.client.pg_con.fetchval(
             """
             INSERT INTO levels.users(user_id, guild_id, lvl, xp)
-            VALUES ($1, $2, 1, 1)
+            VALUES ($1, $2, 0, 0)
             ON CONFLICT (user_id, guild_id) DO
             UPDATE SET xp = users.xp + 1 
             RETURNING xp
@@ -54,14 +60,15 @@ class Levels(commands.Cog):
         # print(f"EXAMPLE: {lvltoxp(4)}")
         # print(f"EXAMPLE: {lvltoxp(5)}")
         # decimal.Decimal(((5 / 4) * xp) ** (1 / 3))
-        print(f"1/3: {decimal.Decimal(1 / 3)}")
-        print(f"ACTUAL LVL: {((5 / 4) * float(xp)) ** (1/3)}")
-        print(f"XPTOLVL: {xptolvl(100)}")
+        print(f"ACTUAL LVL: {math.floor(((5/4)*xp)**(1/3))}")
+
         print(f"xp: {xp}")
         print(f"lvl: {xptolvl(xp)}")
-        print(self.is_lvl_up(xp, lvl))
-        if self.is_lvl_up(xp, lvl):
+        if self.is_lvl_up(xp, lvl) and lvl > 0:
+            print(True)
             await message.channel.send(f"{message.author.mention} is now level {lvl}")
+        else:
+            print(False)
 
 
 def setup(client):
